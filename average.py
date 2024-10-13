44a5844293fe46933b406981afce1bd480861c48
Daksh-coder-oss <vv0039190@gmail.com>
2024-10-13 10:14:27 +0100

    Add files via upload

---
+++ average.py
@@ -0,0 +1,8 @@
+tree1=91
+tree2=100
+tree3=104
+tree4=105
+tree5=107
+sum= tree1+tree2+tree3+tree4+tree5
+average=sum/5
+print("The average of all the trees is :",average)
---
+++ countNotes.py
@@ -0,0 +1,6 @@
+amount=600
+poundnotes50=600//50
+poundnotes20=(600%50)//20
+poundnotes10=(600%20)//10
+poundnotes5=(600%10)//5
+print("Amount of 50 pound notes:",poundnotes50,"amount of 20 pound notes:",poundnotes20,"Amount of 10 pound notes:",poundnotes10,"Amount of 5 pound notes:",poundnotes5)
